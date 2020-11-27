65ff861d3a060a59a08b7b74633fa12b82409b52
Surendra Selvakumar <surenplanet@gmail.com>
2020-11-27 16:57:04 +0800

    New projects

---
+++ Storage/CosmosDB/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace CosmosDB
+{
+    class Program
+    {
+        private static readonly string _endpointUri = "https://coursedatabase502.documents.azure.com:443/";
+        private static readonly string _primaryKey = "2HUq2xnTlFj1tNPMeZyoJuBnNqAQPwB9lWfEMkte32pevOYrO3fSZhWfJjPGDyxgMaEj1xx7C7Cd2NMYEwgdVg==";
+        public static async Task Main(string[] args)
+        {
+            using (CosmosClient client = new CosmosClient(_endpointUri, _primaryKey))
+            {
+                DatabaseResponse databaseResponse = await client.CreateDatabaseIfNotExistsAsync("Products");
+                Database targetDatabase = databaseResponse.Database;
+                await Console.Out.WriteLineAsync($"Database Id:\t{targetDatabase.Id}");
+            }
+        }
+    }
+}
